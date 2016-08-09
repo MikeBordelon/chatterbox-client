d0011c168d0337501337eddaaa48a74401f061cb
MikeBordelon <374mike@gmail.com>
2016-08-08 18:41:23 -0700

    Pass all tests up to 2nd chatroom bevahior

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,99 @@
-// YOUR CODE HERE:
+// var username = window.location.search.split('=')[1];
+// if (username.indexOf('%20') > -1) {
+//   username = username.split('%20').join(' ');
+// }
+
+// var message = {
+//   username: username,
+//   text: 'default msg',
+//   roomname: 'default'
+// };
+
+var app = {
+  server: 'https://api.parse.com/1/classes/messages'
+};
+
+app.init = function () {};
+
+app.send = function (message) {
+  $.ajax({
+    url: app.server,
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('Successfully sent msg!');
+    }, 
+    error: function (data) {
+      console.log('Failed to send message:', data);
+    }
+  });
+};
+
+app.fetch = function () {
+  $.ajax({
+  // This is the url you should use to communicate with the parse API server.
+    url: app.server,
+    type: 'GET',
+    //data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      data.results.forEach(function(message) {
+        var $text = message.username + ': ' + message.text;
+        var $message = $('<div></div>').text($text);
+        
+        // console.log(message)
+        $('#chats').append($message);
+      });
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message', data);
+    }
+  });
+};
+
+app.clearMessages = function () {
+  $('#chats').empty();
+};
+
+app.addMessage = function (message) {
+  $.ajax({
+    url: app.server,
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('Successfully sent msg!');
+    }, 
+    error: function (data) {
+      console.log('Failed to send message:', data);
+    }
+  });  
+};
+
+app.addRoom = function () {};
+
+setInterval(app.fetch(), 500);
+// $.ajax({
+//   // This is the url you should use to communicate with the parse API server.
+//   url: 'https://api.parse.com/1/classes/messages',
+//   type: 'GET',
+//   // data: JSON.stringify(),
+//   contentType: 'application/json',
+//   success: function (data) {
+//     data.results.forEach(function(message) {
+//       var $text = message.username + ': ' + message.text;
+//       var $message = $('<div></div>').text($text);
+      
+//       // console.log(message)
+//       $('#chats').append($message);
+//     });
+//     console.log('chatterbox: Messages recieved');
+//   },
+//   error: function (data) {
+//     // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+//     console.error('chatterbox: Failed to send message', data);
+//   }
+// });
 
